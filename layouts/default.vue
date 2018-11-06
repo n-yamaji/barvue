c928b51b5f649c2bba84830fa540d06906bff483
nobuaki.yamaji <nobuaki.yamaji@monopos.co.jp>
2018-11-07 01:06:47 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app light>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'barvue'
+      }
+    }
+  }
+</script>
