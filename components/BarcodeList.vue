008a7b6d3078b107cf355053759340b2e2f7b247
nobuaki.yamaji <nobuaki.yamaji@monopos.co.jp>
2018-11-29 13:09:40 +0900

    add page barcode list printing

---
+++ components/BarcodeList.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-flex>
+        <v-layout 
+          row 
+          wrap
+          justify-space-between
+          grid-list-xl>
+          <v-flex 
+            v-for="barcode in barcodes" 
+            :key="barcode.code"
+            xs6
+            sm6
+            md6>
+            <p>{{ barcode.label }}</p>
+            <p>¥{{ barcode.retailPrice }}</p>
+            <barcode 
+              :value="barcode.code" 
+              :options="{ 
+                format: format,
+                valid: function() { return true; }
+              }" 
+              tag="canvas"/>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import VueBarcode from '@chenfengyuan/vue-barcode';
+
+export default {
+  components: {
+    'barcode': VueBarcode
+  },
+  props: {
+    format: {
+      type: String,
+      default: 'EAN13'
+    },
+    barcodes: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
---
+++ pages/print.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container 
+    fluid
+    grid-list-md>
+    <barcode-list
+      :barcodes="barcodesArray"
+      :format="selectedFormat"/>
+    <v-select
+      v-model="selectedFormat"
+      :items="supportedFormats"
+      box
+      label="バーコードの種類"/>
+    <v-textarea
+      v-model="barcodes"
+      box
+      label="バーコード（複数可）"
+      auto-grow/>
+    <v-btn @click="print()">Print</v-btn>
+  </v-container>
+</template>
+
+<script>
+import VueBarcode from '@chenfengyuan/vue-barcode';
+import BarcodeList from '~/components/BarcodeList.vue'
+
+export default {
+  components: {
+    'barcode': VueBarcode,
+    BarcodeList
+  },
+  data: () => ({
+    barcodes: 'い・ろ・は・す 555ML PETx24	4902102091862	1,317\n\
+綾鷹 2L PETx6	4902102112208	585\n\
+からだ巡茶 からだすこやか茶W 350ML PETx24	4902102108072	2,843',
+    selectedFormat: 'EAN13',
+    supportedFormats: [
+      'CODE128', 
+      'CODE128A', 
+      'CODE128B', 
+      'CODE128C', 
+      'EAN13', 
+      'EAN8', 
+      'EAN5', 
+      'EAN2', 
+      'UPC',
+      'CODE39', 
+      'ITF14', 
+      'MSI', 
+      'MSI10', 
+      'MSI11', 
+      'MSI1010', 
+      'MSI1110', 
+      'pharmacode', 
+      'codabar', 
+    ]
+  }),
+  computed: {
+    barcodesArray() {
+      if (this.barcodes == null) { 
+        return [];
+      }
+
+      var lines = this.barcodes.split('\n');
+      return lines.map((line) => {
+        var datas = line.split('\t');
+        return {
+          label: datas[0],
+          code: datas[1],
+          retailPrice: datas[2]
+        }
+      })
+    }
+  },
+  methods: {
+    print() {
+      window.print();
+    }
+  }
+}
+</script>
