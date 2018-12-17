80a46674bae40157c4e4c551c6fbcdf1be73e9fd
nobuaki.yamaji <nobuaki.yamaji@monopos.co.jp>
2018-12-18 02:50:54 +0900

    add navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,36 @@
 <template>
   <v-app light>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :to="item.to"
+          :key="i"
+          router
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon" />
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-toolbar
       :clipped-left="clipped"
       fixed
       app
     >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
       <v-toolbar-title v-text="title"/>
     </v-toolbar>
     <v-content>
@@ -26,11 +52,14 @@
     data() {
       return {
         clipped: false,
-        drawer: true,
+        drawer: false,
         fixed: false,
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
+        items: [
+          { icon: 'grain', title: 'Pattern', to: '/' },
+          { icon: 'grid_on', title: 'Grid', to: '/grid' },
+          { icon: 'list', title: 'List', to: '/list' }
+        ],
+        miniVariant: true,
         title: 'barvue'
       }
     }
